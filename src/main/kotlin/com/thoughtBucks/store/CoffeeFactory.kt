a7356515fe1f97c5ece7944d39b127084fb1d4ac
fanchaokong <finlaykong@126.com>
2021-02-27 17:15:12 +0800

    [Fanchao]: condition

--- src/main/kotlin/com/thoughtBucks/store/CoffeeFactory.kt
+++ src/main/kotlin/com/thoughtBucks/store/CoffeeFactory.kt
@@ -9,9 +9,9 @@ import com.thoughtBucks.order.CoffeeOrder
 class CoffeeFactory {
     fun makeCoffee(order: CoffeeOrder): Coffee {
         return when (order.type) {
-            "Latte" -> Latte(size = order.size)
-            "Cappuccino" -> Cappuccino(size = order.size)
-            "Long Black" -> LongBlack(size = order.size)
+            "Latte" -> Latte(size = order.size, sugar = order.sugar)
+            "Cappuccino" -> Cappuccino(size = order.size, sugar = order.sugar)
+            "Long Black" -> LongBlack(size = order.size, sugar = order.sugar)
             else -> throw IllegalArgumentException("Unknown coffee type")
         }
     }
